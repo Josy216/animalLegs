b238314793ceb60addb1c32236f836a85fdcee2a
Joseph Teka <179788482+Josy216@users.noreply.github.com>
2025-02-21 01:48:30 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let chicken = document.getElementById("chicken");
+let cow = document.getElementById("cow");
+let pig = document.getElementById("pig");
+let display = document.getElementById("demo");
+let btn = document.querySelector("button");
+let togle = document.querySelector(".display");
+
+btn.addEventListener("click", function animalLeg() {
+  togle.style.display = "block";
+  let animalLegs = 2 * chicken.value + 4 * cow.value + 4 * pig.value;
+  display.innerHTML = `Your animal Total legs are : ${animalLegs}`;
+});
