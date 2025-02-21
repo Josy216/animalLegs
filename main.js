1033d615e0399d1b8ce6ae46eebb5a35e24ff8ca
Joseph Teka <179788482+Josy216@users.noreply.github.com>
2025-02-21 11:23:31 +0300

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,19 @@ let togle = document.querySelector(".display");
 
 btn.addEventListener("click", function animalLeg() {
   togle.style.display = "block";
+  let inputs = [chicken, cow, pig];
+  let invalid = inputs.some(input => input.value < 0);
+
+  inputs.forEach(input => input.classList.remove("error"));
+  display.classList.remove("error");
+
+  if (invalid) {
+    display.innerHTML = "Please enter positive numbers. Legs cannot be negative!";
+    display.classList.add("error");
+    inputs.forEach(input => { if (input.value < 0) input.classList.add("error"); });
+    return;
+  }
+
   let animalLegs = 2 * chicken.value + 4 * cow.value + 4 * pig.value;
   display.innerHTML = `Your animal Total legs are : ${animalLegs}`;
 });
